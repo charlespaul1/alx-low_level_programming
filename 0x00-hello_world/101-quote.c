14c523534b8dc2c3aa9bebe77c98006234ae54f8
charlespaul1 <you@example.ccharlespaulwabomba@gmail.com>
2022-06-30 10:59:48 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - prints the exact statement and that piece of art is useful
  * Return: 0 (succes)
  */
-int main (void)
+int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
