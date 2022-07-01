dbc205a9172a3dfdab001e1fcf26a0241abc780f
charlespaul1 <you@example.ccharlespaulwabomba@gmail.com>
2022-07-01 06:46:53 -0700

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,16 +5,17 @@
 /**
  * main - prints all possible combo of single digit no.
  *
- * Return : 0
+ * Return: 0
  */
 
 int main(void)
 {
 	int ch;
+
 	for (ch = 48; ch <= 57; ch++)
 	{
 		putchar(ch);
-		if(ch != 57)
+		if (ch != 57)
 		{
 			putchar(44);
 			putchar(32);
