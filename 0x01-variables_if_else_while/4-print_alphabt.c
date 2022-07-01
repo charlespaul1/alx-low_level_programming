f511dabe8ef3fa094410295d657f9b46915b91ef
charlespaul1 <you@example.ccharlespaulwabomba@gmail.com>
2022-07-01 06:00:19 -0700

    printing in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,11 +5,12 @@
 /**
  * main - prints alphabet in capital on new line
  *
- * Return : 0
+ * Return: 0
  */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z' ch++)
 	{
 		if (ch != 'q' && ch != 'e')
