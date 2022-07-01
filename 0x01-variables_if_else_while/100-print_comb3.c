11ed3f9fb395177db4ac4058c88e212cc9c2563e
charlespaul1 <you@example.ccharlespaulwabomba@gmail.com>
2022-07-01 06:52:17 -0700

    combo 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,18 +4,19 @@
 /**
  * main - possible combo of two digits
  *
- * Return : 0
+ * Return: 0
  */
 
 int main(void)
 {
 	int ch;
 	int n;
-	for (ch = 48; ch<= 57; ch++)
+
+	for (ch = 48; ch <= 57; ch++)
 	{
 		for (n = 49; n <= 57; n++)
 		{
-			if(n > ch)
+			if (n > ch)
 			{
 				putchar(ch);
 				putchar(n);
