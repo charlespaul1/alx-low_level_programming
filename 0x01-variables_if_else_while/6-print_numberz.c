18b6f81a65f01307abdf3efdc71b21513971197c
charlespaul1 <you@example.ccharlespaulwabomba@gmail.com>
2022-07-01 06:26:26 -0700

    printing numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,15 @@
-#include <stdlib.h>                                                                                                                     
-#include <time.h>                                                                                                                       
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio>
 /**
  * main - prints single digits of base 10 fro 0
  *
- * Return : 0
+ * Return: 0
  */
 int main(void)
 {
 	int n;
+
 	for (n = 0; n < 10; n++)
 	{
 		printf("%d", n);
