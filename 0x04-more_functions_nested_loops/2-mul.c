331f9982768125343b6b62b5d7e5988f21be182c
charlespaul1 <you@example.ccharlespaulwabomba@gmail.com>
2022-07-07 06:33:42 -0700

    multiplying a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -> multiplies two numbers
+ * @a: integer parameter one
+ * @b: integer parameter two
+ * Return: success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
