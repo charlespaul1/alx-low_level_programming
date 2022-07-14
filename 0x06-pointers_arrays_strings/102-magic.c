e7ffb2524295169bdfb13d4006c7496d256af74a
charlespaul1 <you@example.ccharlespaulwabomba@gmail.com>
2022-07-14 05:51:26 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,8 @@ int main(void)
 	 * write your code here..
 	 * you ara not allowed to code anything else than this line
 	 */
-	*(p + 5) = 98;
+	p[5] = 98;
+	/* so that it prints 98\n*/
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
