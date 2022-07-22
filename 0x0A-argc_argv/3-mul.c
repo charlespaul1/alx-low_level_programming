7ef675f41e65bdac3f9c2fa6187d523b0cad8b1a
charlespaul1 <charlespaulwabomba@gmail.com>
2022-07-22 06:26:59 -0700

    multiplyig two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argc parameter
+ * @argv: an array 
+ * Return: 0 when succesful
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	
+	printf("%d\n", result);
+	return (0);
+}
