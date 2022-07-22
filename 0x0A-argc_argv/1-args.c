f48df2c5acf60868da272468084129cc1b3b820c
charlespaul1 <charlespaulwabomba@gmail.com>
2022-07-22 05:51:53 -0700

    function name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/***
+ * main - a function that prints its name
+ * @argc: arg param
+ * @argv: an array of a command listed
+ * Return: 0 when succesful
+ */
+int main(int argc, char *argv[] _attribute_((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
