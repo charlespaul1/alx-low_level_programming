62bb183185fbd7495b12a48a53e93936d97a6d30
charlespaul1 <charlespaulwabomba@gmail.com>
2022-07-22 04:04:23 -0700

    printing programs name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,10 @@
  * main - prints a programs name followed by a new line
  * @argc: argc parameter
  * @argv: an array of the command listed
- * Return: 0 as succesful 
+ * Return: 0 as succesful
  */
-int main (int argc, char *argv [])
+int main(int argc _attribute_((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
+	return (0);
 }
