c0cbaf8f42fd169f0b74a0933891875ce140c623
charlespaul1 <you@example.ccharlespaulwabomba@gmail.com>
2022-07-22 03:16:41 -0700

    printing a programs name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a programs name followed by a new line
+ * @argc: argc parameter
+ * @argv: an array of the command listed
+ * Return: 0 as succesful 
+ */
+int main (int argc, char *argv [])
+{
+	printf("%s\n", *argv);
+}
