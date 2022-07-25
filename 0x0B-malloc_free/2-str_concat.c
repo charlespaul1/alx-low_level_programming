a43b4d94757c10991034bde27b4c91c05c0dd94e
charlespaul1 <charlespaulwabomba@gmail.com>
2022-07-25 06:45:31 -0700

    concanating 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * str_concat - string concatinating fucntion
+ * str_concat - string concatinating 2 fucntions
  * @s1: first string
  * @s2: second string
  * Return: first string + second string
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, g = 0, h = 0, j = 0;
-	char *s;
+	char *a;
+	int i, j, g, h;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[g])
-		g++;
 
-	h = i + g;
-	s = (char *)malloc(h * sizeof(char) + 1);
-	if (s == NULL)
+	for (i = 0, s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
 		return (NULL);
-	g = 0;
-	while (j < h)
+
+	for (g = 0, h = 0; g < (i + j + 1); g++)
 	{
-		if (j < i)
-			s[j] = s1[j];
-		if (j >= i)
-		{
-			s[j] = s2[g];
-			g++;
-		}
-		j++
+		if (g < i)
+			a[g] = s1[g];
+
+		else
+			a[g] = s2[h++];
 	}
-	s[j] = '\0';
-	return (s);
+
+	return (a);
 }
+
+
