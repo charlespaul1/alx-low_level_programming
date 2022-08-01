5c8a75a851524885bb69f909f54017f670479c37
charlespaul1 <charlespaulwabomba@gmail.com>
2022-08-01 05:47:17 -0700

    new dog fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,46 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
-dog_t *new_dog(char *name, float age, char *owner);
-
-/**
- * _strlen - checks the length of a string
- * @str: string to be measured
- *
- * Return: length of a string
- */
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (*str++)
-		len++;
-
-	return (len);
-}
-
-/**
- * _strcopy - copies a string pointed to by src, including the null
- * character to a buffer pointed to by dest
- * @dest: the buffer storing the string copy
- * @src: soure string
- *
- * Return: pointer to dest
- */
-char *_strcopy(char *dest, char *src)
-{
-	int index = 0;
-
-	for (index = 0; src[index]; index++)
-		dest[index] = src[index];
-
-	dest[index] = '\0';
-
-	return (dest);
-}
-
 /**
  * new_dog - creates new dog
  * @name: name of dog
