1637bd16d4a1a8b8a837b56d0031d9431d36e53c
charlespaul1 <charlespaulwabomba@gmail.com>
2022-08-11 05:14:55 -0700

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,18 +9,47 @@
  * @str: string to be added to new node
  * Return: address of new element else NULL
  */
+
+/**
+ * _strlen - gets length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
 list_t *add_node(list_t **head, const char *str)
 {
-	int len;
-	list_t *new_node = malloc(sizeof(list_t));
+	int i, len;
+	char *content;
+	list_t *new;
 
-	if (new_node == NULL)
+	if (str == NULL || head == NULL)
 		return (NULL);
-	for (len = 0; str[len]; len++;)
+	len = _strlen(str);
+	new = *head;
+	content = malloc((len + 1) * sizeof(char));
+
+	if (content == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++;)
+		content[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL);
+	{
+		free(content);
+		return (NULL);
+	}
 		;
-	new_node->len = len;
-	new_node->str = strdup(str);
-	new_node->next = (*head);
-	(*head) = new_node;
-	return (new_node);
+	new->str = content;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
