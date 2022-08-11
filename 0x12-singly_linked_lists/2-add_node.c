f7abae9413bcd77acfde9663b8b82bad33137015
charlespaul1 <charlespaulwabomba@gmail.com>
2022-08-11 05:53:17 -0700

    adding a node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,47 +9,18 @@
  * @str: string to be added to new node
  * Return: address of new element else NULL
  */
-
-/**
- * _strlen - gets length of string
- * @s: string
- * Return: length of string
- */
-
-int _strlen(const char *s)
-{
-	int i;
-
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
 list_t *add_node(list_t **head, const char *str)
 {
-	int i, len;
-	char *content;
-	list_t *new;
+	int len;
+	list_t *new_node = malloc(sizeof(list_t));
 
-	if (str == NULL || head == NULL)
+	if (new_node == NULL)
 		return (NULL);
-	len = _strlen(str);
-	new = *head;
-	content = malloc((len + 1) * sizeof(char));
-
-	if (content == NULL)
-		return (NULL);
-	for (i = 0; str[i]; i++;)
-		content[i] = str[i];
-	new = malloc(sizeof(list_t));
-	if (new == NULL);
-	{
-		free(content);
-		return (NULL);
-	}
+	for (len = 0; str[len]; len++)
 		;
-	new->str = content;
-	new->len = len;
-	new->next = *head;
-	*head = new;
-	return (new);
+	new_node->len = len;
+	new_node->str = strdup(str);
+	new_node->next = (*head);
+	(*head) = new_node;
+	return (new_node);
 }
