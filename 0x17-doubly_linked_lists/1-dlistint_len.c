1433cbb7d8604d6dddd269ff360b258ab1047a29
charlespaul1 <charlespaulwabomba@gmail.com>
2022-09-29 05:18:13 -0700

    fnding no. of elements in a linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,11 +7,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i;
+	size_t counter = 0;
 
-	if (h == NULL)
-		return (0);
-	for (i = 0; h != NULL i++)
+	while (h != NULL)
+	{
+		counter++;
 		h = h->next;
-	return (i);
+	}
+
+	return (counter);
 }
