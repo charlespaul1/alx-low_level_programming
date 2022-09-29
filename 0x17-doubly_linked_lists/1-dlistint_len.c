6521fb5cc8ca5542f17e4e79dbcb2a870cdf96c6
charlespaul1 <charlespaulwabomba@gmail.com>
2022-09-29 05:10:22 -0700

    returning number of elements in a linked dlistint_t fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,7 +5,7 @@
  * @h: pointer to the head of a linked list
  * Return: number of elements in a linked dlistint_t list.
  */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i;
 
